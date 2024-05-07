b25d7060a8c4aa2eb0e3b4180c059856ad328472
Polabiel <bielgabrieloliveira77@gmail.com>
2024-05-06 22:44:17 -0300

    Flask init in project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CognitiveCode',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/Used-Favorites/CognitiveCode',
+    license='MIT Lincese',
+    author=['Gabriel Oliveira', 'Henrique Nakaema'],
+    author_email='',
+    description=''
+)
